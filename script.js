6f4a26a40668148896796f084e310ca056e43036
Fuowng <223001779@daihocthudo.edu.vn>
2024-10-04 14:28:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sử dụng alert
+alert("Hello world");
+
+// Viết ra màn hình
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.innerHTML += "<h1>Hello world</h1>";
+});
